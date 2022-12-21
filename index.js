aa24451633eebc55a81d4b462697a886b110a779
Swhag <khg5293@outlook.com>
2022-12-21 01:40:55 -0500

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('works');
+
+function fetchData() {
+  fetch('https://Swhag.github.io/testData.json')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+fetchData();
